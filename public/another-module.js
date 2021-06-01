b35030b66076f99e1324138f2e93f5586ed24737
Suong Tran <69496053+Suong-Tran@users.noreply.github.com>
2021-06-01 22:02:44 +0700

    making changes to files

--- public/another-module.js
+++ public/another-module.js
@@ -1,1 +1,14 @@
+var HobbitSettlement = class {
+	constructor(name, founding, founders) {
+		this.name = name;
+		this.founding = founding;
+		this.founders = founders;
+		this.originalSettlement = { name: 'Eriador', details: eriadorDetails };
+	}
+};
 
+var theShire = new HobbitSettlement('The Shire', 1601, ['Marcho', 'Blanco']);
+
+var buckland = new HobbitSettlement('Buckland', 2340, 'Gorhendad Oldbuck');
+
+export{theShire,buckland}
--- public/my-module.js
+++ public/my-module.js
@@ -1,1 +1,7 @@
+import { theShire as oldTheShire,buckland } from './another-script.js';
 
+var theShire = theShire;
+
+var statement = () => `${theShire.name} was founded ${buckland.founding - theShire.founding} years before ${buckland.name}.`;
+
+export{theShire,statement};
